08dbe2113c9de2598e2e772e4827e4dca714cbdc
Wilgnne Khawan <wilgnne.kba@gmail.com>
2022-05-30 18:16:37 +0000

    feat: use cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import passport from 'passport';
+
 import router from './router';
 import './middleware/passport';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
