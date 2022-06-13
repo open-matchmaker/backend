00dbbcb9d4938e9c6eb023bb7716457b3b806987
tonyfroes <tonydanilofroes@gmail.com>
2022-06-13 21:39:37 +0000

    swagger get all users or findusername

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,22 @@
 import { Router } from 'express';
 
+// import swaggerUi from 'swagger-ui-express';
 import user from './user';
 import session from './session';
 import post from './post';
 import game from './game';
 
 const router = Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 router.get('/', (_req, res) => res.send('Hello World! matchmaker-api'));
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.use('/user', user);
 router.use('/session', session);
 router.use('/post', post);
 router.use('/game', game);
+router.use('/api-docs', swaggerUi.serve);
 
 export default router;
